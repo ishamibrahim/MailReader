921943b969af6cc59e76fe8e1684be7fcb8d1a22
ishamibrahim <Isham.Ibrahim@rackspace.com>
2017-09-19 15:52:50 +0530

    adding required packages

--- speaker.py
+++ speaker.py
@@ -5,5 +5,5 @@ import os
 def speak_the_sentence(sentence):
     tts = gTTS(text=sentence, lang='en')
     tts.save("sentence.mp3")
-    os.system("afplay sentence.mp3")
+    os.system("mpg321 sentence.mp3")
 
